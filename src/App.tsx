854acf69b6e5e44c15c5bf558695cf46adc02f5f
Slater8242 <riad.ashrafov@ecenta.com>
2022-12-25 00:51:14 +0200

    input validation by E.164 format and adding grid to components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
+import { Button, Grid } from '@mui/material';
 
 // initialize a GraphQL client
 const client = new ApolloClient({
@@ -44,7 +45,8 @@ function App() {
   const countries: readonly CountryType[] = data?.countries.map((country: CountryType) => country);
   
   useEffect(()=>{
-    data?.countries.map((country: CountryType) => country.phone === phoneNumber ? setCountryName(country.name) : "");
+    data?.countries.map((country: CountryType) => country.phone === phoneNumber.replace("+", "") ? setCountryName(country.name) : "");
+    phoneNumber.match(/^\+\d$/)
     console.log(phoneNumber);
   }, [phoneNumber, data?.countries])
   
@@ -57,48 +59,68 @@ function App() {
   if (loading || error) {
     return <p>{error ? error.message : 'Loading...'}</p>;
   }
+  
+  // console.log(phoneNumber.match(/^\+[1-9]\d{10,14}$/));
+  // console.log(phoneNumber.replace(/\D/,""));
 
   return (
     <>
-      <Autocomplete
-        multiple={false}
-        id="country-select"
-        value={{name: countryName}}
-        sx={{ width: 300, mt: 2, ml: 2 }}
-        options={countries}
-        isOptionEqualToValue={(option, value) => option.name === value.name}
-        onChange={(event: any, newValue: CountryType | null) => newValue === null ? setCountryName("") : setCountryName(newValue.name)}
-        getOptionLabel={(option: CountryType) => option.name}
-        renderOption={(props, option) => (
-          <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
-            <img
-              loading="lazy"
-              width="20"
-              src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-              srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-              alt=""
-            />
-            {option.name} ({option.code}) +{option.phone}
-          </Box>
-        )}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Country"
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'country', // disable autocomplete and autofill
-            }}
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        alignContent="center"
+        alignItems="center" 
+        height="100vh"
+      >
+        <Grid item>
+          <Autocomplete
+            multiple={false}
+            id="country-select"
+            value={{ name: countryName }}
+            sx={{ width: 300 }}
+            options={countries}
+            isOptionEqualToValue={(option, value) => option.name === value.name}
+            onChange={(event: any, newValue: CountryType | null) => newValue === null ? setCountryName("") : setCountryName(newValue.name)}
+            getOptionLabel={(option: CountryType) => option.name}
+            renderOption={(props, option) => (
+              <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+                <img
+                  loading="lazy"
+                  width="20"
+                  src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+                  srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+                  alt=""
+                />
+                {option.name} ({option.code}) +{option.phone}
+              </Box>
+            )}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Country"
+                inputProps={{
+                  ...params.inputProps,
+                  autoComplete: 'country', // disable autocomplete and autofill
+                }}
+              />
+            )}
           />
-        )}
-      />
-      <TextField
-        id="country-input" 
-        sx={{ width: 300, mt: 2, ml: 2 }}
-        label="Your phone number"
-        value={phoneNumber}
-        onChange={(event: ChangeEvent<HTMLInputElement>) => setPhoneNumber(event.target.value)}
-      />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="country-input"
+            sx={{ width: 300 }}
+            label="Your phone number"
+            value={phoneNumber}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => setPhoneNumber(event.target.value.slice(0, 16).replace(/(?!\+)\D/, ""))}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained">Next</Button>
+        </Grid>
+      </Grid>
     </>
   );
 }
